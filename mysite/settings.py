37335e69077259c1a1aaad284ab0a5942be89734
Vivek Kumar Singh <kumarsinghvivek54@gmail.com>
2021-08-27 10:43:49 +0530

    add allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,3 +124,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ['*',
+    '0.0.0.0',
+    'blog12319090.herokuapp.com',
+    '127.0.0.1'
+]
